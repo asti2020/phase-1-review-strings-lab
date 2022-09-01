2337472ce282441ebfee44e310f6de754f6659f0
Astika Luintel <astild2020@gmail.com>
2022-09-01 19:47:11 -0400

    commit tree 5c2243a9c2599108972d8877fabdd57174c7fc98

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+let currentUser = "ameeee";
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+let shortGreeting = `Welcome,  ${currentUser.slice(0,2) } !`;
